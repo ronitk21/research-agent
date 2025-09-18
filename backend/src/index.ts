b21af81c73e442709a564c73c80b07d0494dfed1
Ronit Kedia <kediaronit.21@gmail.com>
2025-09-18 18:32:38 +0530

    feat: implement basic Express server with dotenv

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: "true",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on Port: ${PORT}`);
+});
