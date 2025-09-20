d0320235a53573f6f5a52ee0f947f754ef1a725f
Ronit Kedia <kediaronit.21@gmail.com>
2025-09-20 11:54:35 +0530

    refactor: update index.ts to include structured error handling middleware and improve CORS configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 
 import researchRouter from "./routes/research.route";
 import "./workers/researchWorker";
+import errorHandler, { notFound } from "../middleware";
 
 dotenv.config();
 
@@ -11,15 +12,22 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
 
 app.get("/", (req, res) => {
   res.status(200).json({
-    success: "true",
+    success: true,
   });
 });
 
 app.use("/api/v1", researchRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on Port: ${PORT}`);
